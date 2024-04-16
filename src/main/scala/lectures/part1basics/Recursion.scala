00b9b9f8537fbc28607a17dbe9019feaf9e3d1dc
Elar Saks <elarsaks@gmail.com>
2024-04-16 11:24:59 +0300

    Do the exercises.

--- src/main/scala/lectures/part1basics/Recursion.scala
+++ src/main/scala/lectures/part1basics/Recursion.scala
@@ -45,4 +45,40 @@ object Recursion extends App{
     2. IsPrime function tail recursive
     3. Fibonacci function
    */
+
+  @scala.annotation.tailrec
+  def concatenateTailRec(aString: String, n: Int, accumulator: String): String =
+    if (n <= 0) accumulator
+    else concatenateTailRec(aString, n-1, aString + accumulator)
+
+  println(concatenateTailRec("hello ", 3, ""))
+
+
+
+  def isPrime(n: Int): Boolean = {
+    @scala.annotation.tailrec
+    def isPrimeTailRec(t: Int, isStillPrime: Boolean): Boolean =
+      if(!isStillPrime) false
+      else if(t <= 1) true
+      else isPrimeTailRec(t-1, n % t != 0 && isStillPrime)
+
+    isPrimeTailRec(n / 2, true)
+  }
+
+  println(isPrime(37))
+  println(isPrime(2003))
+  println(isPrime(2001))
+
+  def fibonacci(n: Int): Int = {
+    @scala.annotation.tailrec
+    def fiboTailrec(i: Int, last: Int, nextToLast: Int): Int =
+      if (i >= n) last
+      else fiboTailrec(i + 1, last + nextToLast, last)
+
+    if (n <= 2) 1
+    else fiboTailrec(3, 1, 1)
+  }
+  
+  println(fibonacci(8))
+
 }
