2dddbc9a9fe4eeadc3cc957ceeff0515bebfe1e6
Elar Saks <elarsaks@gmail.com>
2024-04-21 09:46:58 +0300

    Get started with imports.

--- src/main/scala/lectures/part2oop/packagingAndImports.scala
+++ src/main/scala/lectures/part2oop/packagingAndImports.scala
@@ -1,5 +1,21 @@
 package lectures.part2oop
+import playground.Cinderella
 
 object packagingAndImports extends App {
-  val writer = new Writer("Daniel", "RockTheJVM", 2018)
+
+  // Package members are accessible by their simple name
+  // OOBasics.Author
+  val writer = new Author("Daniel", "RockTheJVM", 2018)
+
+  // import the package
+  val princess = new Cinderella // playground.Cinderella = fully qualified name
+
+  // packages are in hierarchy
+  // matching folder structure.
+
+  // package object
+  sayHello
+  println(SPEED_OF_LIGHT)
+
+  // imports
 }
