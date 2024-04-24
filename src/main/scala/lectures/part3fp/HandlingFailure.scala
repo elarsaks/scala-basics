e53e21a8eecc54c261316c2e6fc51b11da994cc8
Elar Saks <elarsaks@gmail.com>
2024-04-24 12:54:04 +0300

    Learn Exceptions.

---
+++ src/main/scala/lectures/part3fp/HandlingFailure.scala
@@ -0,0 +1,69 @@
+package lectures.part3fp
+
+import scala.util.{Try, Failure, Success}
+import java.util.Random
+
+object HandlingFailure extends App {
+  // create success and failure
+  val aSuccess = Success(3)
+  val aFailure = Failure(new RuntimeException("SUPER FAILURE"))
+
+  println(aSuccess)
+  println(aFailure)
+
+  def unsafeMethod(): String = throw new RuntimeException("NO STRING FOR YOU BUSTER")
+
+  // Try objects via the apply method
+  val potentialFailure = Try(unsafeMethod())
+  println(potentialFailure)
+
+  // syntax sugar
+  val anotherPotentialFailure = Try {
+    // Code that might throw
+  }
+
+  // utilities
+  println(potentialFailure.isSuccess)
+
+  // orElse
+  def backupMethod(): String = "A valid result"
+  val fallbackTry = Try(unsafeMethod()).orElse(Try(backupMethod()))
+  println(fallbackTry)
+
+  // IF you design the API
+  def betterUnsafeMethod(): Try[String] = Failure(new RuntimeException())
+  def betterBackUpMethod(): Try[String] = Success("A valid result")
+  val betterFallback = betterUnsafeMethod() orElse betterBackUpMethod()
+
+  // map, flatMap, filter
+  println(aSuccess.map(_ * 2))
+  println(aSuccess.flatMap(x => Success(x * 10)))
+  println(aSuccess.filter(_ > 10))
+  // => for-comprehension
+
+  /*
+    Exercise
+   */
+  val hostname ="localhost"
+  val port = "8080"
+  def renderHTML(page: String) = println(page)
+
+  class Connection {
+    def get(url: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html>...</html>"
+      else throw new RuntimeException("Connection interrupted")
+    }
+  }
+
+  object HttpService {
+    val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection =
+      if(random.nextBoolean()) new Connection
+      else throw new RuntimeException()
+  }
+
+  // If you get the html page from the connection, print it to the console i.e. call renderHTML
+  
+}
